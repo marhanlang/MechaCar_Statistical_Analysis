b05affd9a5b96ea71449c2ed4238bdcf007013ec
Margaret Lang <78670979+marhanlang@users.noreply.github.com>
2021-06-19 16:50:46 -0500

    Add Suspension Coil PSI Summary tables

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
+# Deliverable 1
+
 library(dplyr)
 
 library(readr)
@@ -7,3 +9,13 @@ View(MechaCar_mpg)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_mpg) #create linear model
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_mpg)) #generate summary statistics
+
+# Deliverable 2 ---------------------------------
+
+library(readr)
+Suspension_Coil <- read_csv("Suspension_Coil.csv")
+View(Suspension_Coil)
+
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD = sd(PSI), .groups = 'keep') #create summary table with multiple columns
+
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD = sd(PSI), .groups = 'keep') #create summary table
