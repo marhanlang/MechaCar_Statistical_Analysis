628abe976502d46672d26d1c2cc621b9e2c97e3d
Margaret Lang <78670979+marhanlang@users.noreply.github.com>
2021-06-19 19:08:13 -0500

    Update RScript with t-test

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -19,3 +19,16 @@ View(Suspension_Coil)
 total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD = sd(PSI), .groups = 'keep') #create summary table with multiple columns
 
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD = sd(PSI), .groups = 'keep') #create summary table
+
+# Deliverable 3 ---------------------------------
+
+t.test(Suspension_Coil$PSI, mu = 1500)
+
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot1")$PSI, mu=1500)
+
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot2")$PSI, mu=1500)
+
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot3")$PSI, mu=1500)
+
+# Deliverable 4 ----------------------------------
+
